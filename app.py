9a1ef52d90f49149f5e273f93b98f4f025b03efc
Julius Sky <julius.skye@gmail.com>
2019-12-28 17:40:57 +1100

    make pymath show object images
    make pymath show object images

--- app.py
+++ app.py
@@ -2,14 +2,35 @@ from flask import Flask, flash, redirect, render_template, request, session, abo
 
 app = Flask(__name__)
 
+def getobjectimg(name, url="/"):
+    if name == "Hexahedron":
+        url = "https://upload.wikimedia.org/wikipedia/commons/a/a5/Hexahedron.svg"
+    elif name == "Octahedron":
+        url = "https://upload.wikimedia.org/wikipedia/commons/0/07/Octahedron.svg"
+    elif name =="Tetrahedron":
+        url = "https://upload.wikimedia.org/wikipedia/commons/f/fc/Tetrahedron.svg"
+    elif name == "Icosahedron":
+        url = "https://upload.wikimedia.org/wikipedia/commons/b/b7/Icosahedron.svg"
+    elif name == "Dodecahedron":
+        url = "https://upload.wikimedia.org/wikipedia/commons/a/a4/Dodecahedron.svg"
+    else:
+        url = "/"
+    return url
+
 @app.route("/")
 def index():
     return "Flask App!"
 
-@app.route("/hello/<string:name>/")
-def hello(name):
+@app.route("/pymath/")
+def math():
     return render_template(
-    'test.html',name=name)
+    'index.html')
+
+@app.route("/pymath/<string:name>/")
+def object(name):
+    print(name)
+    return render_template(
+    'object.html',name=name, imgurl=getobjectimg(name))
 
 
 if __name__ == "__main__":
